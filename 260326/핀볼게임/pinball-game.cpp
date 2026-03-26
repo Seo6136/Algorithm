c21e168ec612dd303052d6b585729b0e939132cf
서지원 <80915887+Seo6136@users.noreply.github.com>
2026-03-26 23:29:00 +0900

    Title: 핀볼게임, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260326/핀볼게임/pinball-game.cpp
+++ 260326/핀볼게임/pinball-game.cpp
@@ -13,8 +13,10 @@ int out_time(int y, int x, int dir) {
         sec++;
         int ny = y + dy[dir];
         int nx = x + dx[dir];
+        //cout << ny << " " << nx << '\n';
 
         if (ny > n-1 || ny < 0 || nx > n-1 || nx < 0) {
+            //cout << sec << '\n';
             return sec;
         }
 
@@ -72,7 +74,7 @@ int main() {
     //down
     for (int i = 0; i<n; i++) {
         int dir = 0;
-        int y = n-1;
+        int y = n;
         int x = i;
         int sec = out_time(y,x,dir);
         if (sec > maxsec) maxsec = sec;
@@ -81,7 +83,7 @@ int main() {
     for (int i = 0; i<n; i++) {
         int dir = 3;
         int y = i;
-        int x = n-1;
+        int x = n;
         int sec = out_time(y,x,dir);
         if (sec > maxsec) maxsec = sec;
     }
