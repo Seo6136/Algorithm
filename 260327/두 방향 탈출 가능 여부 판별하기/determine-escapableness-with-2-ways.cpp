828aadf8a55545287e208ec181d45b495b243215
서지원 <80915887+Seo6136@users.noreply.github.com>
2026-03-27 16:10:05 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260327/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 260327/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -1,43 +1,29 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
 int n, m;
-int maze [100][100];
-bool flag;
-int dy[2] = {1,0};
-int dx[2] = {0,1};
-
-void dfs(int y, int x) {
-    if (y == n-1 && x == m-1) flag = true;
-    if (flag) return;
-
-    for (int i = 0; i<2; i++) {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        //cout << ny << " " << nx << '\n';
-
-        if (ny < 0 || ny > n-1 || nx < 0 || nx > m-1 || maze[ny][nx] == 0) continue;
-        dfs(ny,nx);
-        if (flag) return;
-    }
-}
+int maze[100][100];
+int dp[100][100];
 
 int main() {
     cin >> n >> m;
-    flag = false;
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j<m; j++) cin >> maze[i][j];
+        for (int j = 0; j < m; j++) {
+            cin >> maze[i][j];
+        }
     }
 
+    dp[0][0] = 1;
 
-    // Please write your code here.
-    dfs(0,0);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (maze[i][j] == 0) continue;
+            if (i > 0 && dp[i - 1][j]) dp[i][j] = 1;
+            if (j > 0 && dp[i][j - 1]) dp[i][j] = 1;
+        }
+    }
 
-    if (flag) cout << 1;
-    else cout << 0;
-
+    cout << dp[n - 1][m - 1];
     return 0;
 }
