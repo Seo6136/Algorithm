4b2b9f871b2829b5f9ca56e018a167c4e17e4204
서지원 <80915887+Seo6136@users.noreply.github.com>
2026-03-27 13:53:41 +0900

    Title: 개인정보, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260327/개인정보/personal-info.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct inf {
+    string name;
+    int h;
+    double w;
+};
+
+struct cmp1 {
+    bool operator() (const inf &a, const inf &b) const {
+        return a.h > b.h;
+    }
+};
+struct cmp2 {
+    bool operator() (const inf &a, const inf &b) const {
+        return a.name < b.name;
+    }
+};
+
+inf arr[5];
+
+int main() {
+    // Please write your code here.
+
+
+    for (int i = 0; i<5; i++) {
+        cin >> arr[i].name >> arr[i].h >> arr[i].w;
+    }
+
+    sort(arr, arr+5, cmp2());
+
+    cout << "name" << '\n';
+    for (int i = 0; i<5; i++) {
+        cout << arr[i].name << " " << arr[i].h << " " << arr[i].w << '\n';
+    }
+
+    cout << '\n';
+    sort(arr, arr+5, cmp1());
+
+    cout << "height" << '\n';
+    for (int i = 0; i<5; i++) {
+        cout << arr[i].name << " " << arr[i].h << " " << arr[i].w << '\n';
+    }
+    return 0;
+}
