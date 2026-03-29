79ae5bd0ec75a94b10bdf1b3bc843d9b692500f1
서지원 <80915887+Seo6136@users.noreply.github.com>
2026-03-29 16:22:48 +0900

    Title: 2차원 폭발 게임, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260329/2차원 폭발 게임/The-2D-bomb-game.cpp
+++ 260329/2차원 폭발 게임/The-2D-bomb-game.cpp
@@ -94,23 +94,50 @@ int main() {
     }
 
     int ans = 0;
-    //delete bombs
-    for (int x = 0; x<n; x++) {
-        for (int y = 0; y<n; y++) {
+    bool flag = true;
+    while (flag) {
+        /*for (int i = 0; i<n; i++) {
+            for (int j = 0; j<n; j++) cout << grid[i][j] << " ";
+            cout << '\n';
+        }*/
+        //cout << '\n';
+        flag = false;
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
             int num = grid[y][x];
             if (num == 0) continue;
-            int cnt = 1;
 
-            for (int ny = y+1; ny < n;  ny++) {
-                if (num == grid[ny][x]) cnt++;
+            int start = y;
+            int end = y;
+
+            for (int ny = y + 1; ny < n; ny++) {
+                if (grid[ny][x] == num) end = ny;
                 else break;
             }
 
-            if (cnt >= m) {
-                for (int i = 0; i < cnt; i++) grid[y+i][x] = 0;
+            if (end - start + 1 >= m) {
+                flag = true;
+                for (int i = start; i <= end; i++) grid[i][x] = 0;
+            }
+
+            y = end;
             }
         }
+        //apply gravity
+        for (int x = 0; x<n; x++) {
+            vector<int> tmp;
+            int zero = 0;
+            for (int y = n-1; y>=0; y--) {
+                if (grid[y][x] != 0) tmp.push_back(grid[y][x]);
+                else zero++;
+            }
+            for (int t = 0; t<tmp.size(); t++) {
+                grid[n-1-t][x] = tmp[t];
+            }
+            for (int y = 0; y<zero; y++) grid[y][x] = 0;
+        }
     }
+
 
     for (int i = 0; i<n; i++) {
         for (int j = 0; j<n; j++) {
