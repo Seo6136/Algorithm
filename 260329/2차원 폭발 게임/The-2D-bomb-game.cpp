ee46c88cb4f9ff3e48118a701e55ed453890faa5
서지원 <80915887+Seo6136@users.noreply.github.com>
2026-03-29 14:46:45 +0900

    Title: 2차원 폭발 게임, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260329/2차원 폭발 게임/The-2D-bomb-game.cpp
+++ 260329/2차원 폭발 게임/The-2D-bomb-game.cpp
@@ -8,22 +8,29 @@ int grid[100][100];
 
 void do_it() {
     //delete bombs
-    for (int x = 0; x<n; x++) {
-        for (int y = 0; y<n; y++) {
-            int num = grid[y][x];
-            if (num == 0) continue;
-            int cnt = 1;
+    bool flag = true;
+    while (flag) {
+        flag = false;
+        for (int x = 0; x<n; x++) {
+            for (int y = 0; y<n; y++) {
+                int num = grid[y][x];
+                if (num == 0) continue;
+                int cnt = 1;
 
-            for (int ny = y+1; ny < n;  ny++) {
-                if (num == grid[ny][x]) cnt++;
-                else break;
-            }
+                for (int ny = y+1; ny < n;  ny++) {
+                    if (grid[ny][x] == 0) continue;
+                    if (num == grid[ny][x]) cnt++;
+                    else break;
+                }
 
-            if (cnt >= m) {
-                for (int i = 0; i < cnt; i++) grid[y+i][x] = 0;
+                if (cnt >= m) {
+                    flag = true;
+                    for (int i = 0; i < cnt; i++) grid[y+i][x] = 0;
+                }
             }
         }
     }
+    
     //apply gravity
     for (int x = 0; x<n; x++) {
         vector<int> tmp;
