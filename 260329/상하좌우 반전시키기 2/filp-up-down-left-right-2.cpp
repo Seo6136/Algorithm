d80d1470bf6139c24da22353ad9f8f7ad6a15b46
서지원 <80915887+Seo6136@users.noreply.github.com>
2026-03-29 23:48:22 +0900

    Title: 상하좌우 반전시키기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260329/상하좌우 반전시키기 2/filp-up-down-left-right-2.cpp
+++ 260329/상하좌우 반전시키기 2/filp-up-down-left-right-2.cpp
@@ -4,36 +4,33 @@
 
 using namespace std;
 
-int n,m;
+int n, m;
 vector<vector<int>> grid;
-int dy[5] = {0,-1,0,1,0};
-int dx[5] = {0,0,1,0,-1};
+int dy[5] = {0, -1, 0, 1, 0};
+int dx[5] = {0, 0, 1, 0, -1};
 
-void change_grid(int y, int x, vector<vector<int>> &grid) {
-    for (int i = 0; i<5; i++) {
+void change_grid(int y, int x, vector<vector<int>>& board) {
+    for (int i = 0; i < 5; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
 
-        if (ny < 0 || ny > n-1 || nx < 0 || nx > m-1) continue;
-        if (grid[ny][nx] == 1) grid[ny][nx] = 0;
-        else grid[ny][nx] = 1;
+        if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+        board[ny][nx] ^= 1;
     }
 }
 
 int check_grid(vector<vector<int>> board, int click) {
-    for (int y = 1; y<n; y++) {
-        for (int x = 0; x<m; x++) {
-            if (board[y-1][x] == 0) {
-                change_grid(y,x,board);
+    for (int y = 1; y < n; y++) {
+        for (int x = 0; x < m; x++) {
+            if (board[y - 1][x] == 0) {
+                change_grid(y, x, board);
                 click++;
             }
         }
     }
 
-    for (int y = 0; y<n; y++) {
-        for (int x = 0; x<m; x++) {
-            if (board[y][x] == 0) return INT_MAX;
-        }
+    for (int x = 0; x < m; x++) {
+        if (board[n - 1][x] == 0) return INT_MAX;
     }
 
     return click;
@@ -41,25 +38,32 @@ int check_grid(vector<vector<int>> board, int click) {
 
 int main() {
     cin >> n >> m;
-    grid = vector<vector<int>> (n, vector<int>(m));
+    grid = vector<vector<int>>(n, vector<int>(m));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
     int minclick = INT_MAX;
 
-    for (int i = 0; i<n; i++) {
-        for (int j = 0; j<m; j++) cin >> grid[i][j];
-    }
-    
-    for (int i = 0; i<m; i++) {
-        //non-click
-        int click = check_grid(grid,0);
-        minclick = min(click, minclick);
-        //click
-        change_grid(0,i,grid);
-        click = check_grid(grid,1);
-        minclick = min(click, minclick);
-        change_grid(0,i,grid);
+    for (int mask = 0; mask < (1 << m); mask++) {
+        auto board = grid;
+        int click = 0;
+
+        for (int x = 0; x < m; x++) {
+            if (mask & (1 << x)) {
+                change_grid(0, x, board);
+                click++;
+            }
+        }
+
+        minclick = min(minclick, check_grid(board, click));
     }
 
     if (minclick == INT_MAX) cout << -1;
     else cout << minclick;
+
     return 0;
 }
