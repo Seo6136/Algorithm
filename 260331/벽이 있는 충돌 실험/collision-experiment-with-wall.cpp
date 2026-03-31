61abd8fc629e103e3a2a0c89231c6e4295696f57
서지원 <80915887+Seo6136@users.noreply.github.com>
2026-03-31 12:26:20 +0900

    Title: 벽이 있는 충돌 실험, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260331/벽이 있는 충돌 실험/collision-experiment-with-wall.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <set>
+#include <unordred_map>
+#include <vector>
+
+using namespace std;
+
+int n,m,t,seq,tt;
+unordered_map<pair<int,int>, int> marvels;
+vector<vector<int>> visited;
+
+int main() {
+    cin >> t;
+
+    for (int tc = 0; tc<t; t++) {
+        cin >> n >> m;
+        visited = vector<vector<int>> (n, vector<int>(n,0));
+        s
+
+
+    }
+    // Please write your code here.
+    return 0;
+}
