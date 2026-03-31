20a4619407455e3afa8153df66b6174b3350645e
서지원 <80915887+Seo6136@users.noreply.github.com>
2026-03-31 14:17:18 +0900

    Title: 폭탄 떨구기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260331/폭탄 떨구기/drop-the-bomb.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n,k;
+long long mx,mn; 
+long long bombs[50000];
+
+bool check_bombs(long long mid) {
+    int curidx = 0;
+    long long sp,mp;
+
+    for (int i = 0; i<k; i++) {
+        //cout << curidx << '\n';
+        sp = bombs[curidx];
+        mp = sp + mid*2;
+
+        while (mp >= bombs[curidx]) {
+            curidx++;
+            if (curidx >= n) return true;
+        } 
+    }
+
+    return false;
+}
+
+int main() {
+    // Please write your code here.
+    cin >> n >> k;
+    for (int i = 0; i<n; i++) {
+        cin >> bombs[i];
+    }
+
+    sort(bombs, bombs+n);
+
+    long long left = bombs[0];
+    long long right = bombs[n-1];
+    long long mid;
+
+    while(left <= right) {
+        mid = (left+right) / 2;
+        //cout << "mid " << mid << '\n';
+
+        if (check_bombs(mid)) {
+            right = mid - 1;
+        }
+        else left = mid + 1;
+    }
+
+    cout << mid;
+
+    return 0;
+}
