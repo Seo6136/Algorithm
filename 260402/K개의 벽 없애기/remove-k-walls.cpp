47578fbd3c2ce0af743cca0c2a453080630a697d
서지원 <80915887+Seo6136@users.noreply.github.com>
2026-04-02 12:41:28 +0900

    Title: K개의 벽 없애기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260402/K개의 벽 없애기/remove-k-walls.cpp
+++ 260402/K개의 벽 없애기/remove-k-walls.cpp
@@ -1,68 +1,71 @@
 #include <iostream>
-#include <climits>
 #include <queue>
+#include <vector>
+#include <climits>
 
 using namespace std;
 
-struct status{
-    int y,x,bw;
+struct Status {
+    int y, x, bw;
 };
 
+int n, k, r1, c1, r2, c2;
 int grid[100][100];
-int dis[100][100];
-int n,k,r1,c1,r2,c2;
-int dy[4] = {-1,0,1,0};
-int dx[4] = {0,1,0,-1};
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
 
 int main() {
     cin >> n >> k;
 
-    for (int i = 0; i<n; i++) {
-        for (int j = 0; j<n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> grid[i][j];
-            dis[i][j] = INT_MAX;
         }
     }
 
     cin >> r1 >> c1 >> r2 >> c2;
+    r1--, c1--, r2--, c2--;
 
-    r1-=1;
-    c1-=1;
-    r2-=1;
-    c2-=1;
+    vector<vector<vector<int>>> dis(
+        n, vector<vector<int>>(n, vector<int>(k + 1, INT_MAX))
+    );
 
-    queue<status> q;
+    queue<Status> q;
+    q.push({r1, c1, 0});
+    dis[r1][c1][0] = 0;
 
-    q.push({r1,c1,0});
-    dis[r1][c1] = 0;
-
-    while(!q.empty()) {
-        auto s = q.front();
-        int y = s.y;
-        int x = s.x;
-        int w = s.bw;
-        int d = dis[y][x];
+    while (!q.empty()) {
+        auto cur = q.front();
         q.pop();
 
-        for (int i = 0; i<4; i++) {
-            int ny = y+dy[i];
-            int nx = x+dx[i];
+        int y = cur.y;
+        int x = cur.x;
+        int w = cur.bw;
+        int d = dis[y][x][w];
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
             int nd = d+1;
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+
             int nw = w;
+            if (grid[ny][nx] == 1) nw++;
 
-            if (ny < 0 || ny > n-1 || nx < 0 || nx > n-1 || dis[ny][nx] <= nd) continue;
+            if (nw > k) continue;
+            if (dis[ny][nx][nw] <= nd) continue;
 
-            if (grid[ny][nx] == 1) {
-                nw += 1;
-                if (nw > k) continue;
-            }
-            dis[ny][nx] = nd;
-            q.push({ny,nx,nw});
+            dis[ny][nx][nw] = nd;
+            q.push({ny, nx, nw});
         }
     }
 
+    int ans = INT_MAX;
+    for (int w = 0; w <= k; w++) {
+        ans = min(ans, dis[r2][c2][w]);
+    }
 
-    if (dis[r2][c2] == INT_MAX) cout << -1;
-    else cout << dis[r2][c2];
+    cout << (ans == INT_MAX ? -1 : ans);
     return 0;
 }
