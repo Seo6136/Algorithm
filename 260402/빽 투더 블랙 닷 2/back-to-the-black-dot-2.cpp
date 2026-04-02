8d1ff7e3051d4d6b72dde3692fd1f5501d0ecb72
서지원 <80915887+Seo6136@users.noreply.github.com>
2026-04-02 21:47:49 +0900

    Title: 빽 투더 블랙 닷 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260402/빽 투더 블랙 닷 2/back-to-the-black-dot-2.cpp
+++ 260402/빽 투더 블랙 닷 2/back-to-the-black-dot-2.cpp
@@ -4,96 +4,94 @@
 #include <queue>
 
 using namespace std;
-int n,m,r1,r2;
-vector<vector<pair<int,int>>> g;
-int rdist1[100001];
-int rdist2[100001];
+using ll = long long;
+
+int n, m, r1, r2;
+vector<vector<pair<int,ll>>> g;
+ll rdist1[100001];
+ll rdist2[100001];
 
 struct cmp {
-    bool operator() (const pair<int,int> &a, const pair<int,int> &b) const {
+    bool operator() (const pair<int,ll> &a, const pair<int,ll> &b) const {
         return a.second > b.second;
     }
 };
 
 int main() {
     cin >> n >> m;
-    fill(rdist1,rdist1+n+1,INT_MAX);
-    fill(rdist2,rdist2+n+1,INT_MAX);
+    fill(rdist1, rdist1 + n + 1, LLONG_MAX);
+    fill(rdist2, rdist2 + n + 1, LLONG_MAX);
     cin >> r1 >> r2;
 
-    g = vector<vector<pair<int,int>>>(n+1);
+    g = vector<vector<pair<int,ll>>>(n + 1);
 
-    for (int i = 0; i<m; i++) {
-        int a,b,d;
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        ll d;
         cin >> a >> b >> d;
-        g[a].push_back({b,d});
-        g[b].push_back({a,d});
+        g[a].push_back({b, d});
+        g[b].push_back({a, d});
     }
 
-    int ans = INT_MAX;
-
-    //finf red dist
-    priority_queue<pair<int,int>, vector<pair<int,int>>, cmp> q;
-    q.push({r1,0});
+    priority_queue<pair<int,ll>, vector<pair<int,ll>>, cmp> q;
+    q.push({r1, 0});
     rdist1[r1] = 0;
 
-    while(!q.empty()) {
+    while (!q.empty()) {
         int now = q.top().first;
-        int dis = q.top().second;
+        ll dis = q.top().second;
         q.pop();
 
         if (rdist1[now] != dis) continue;
-        for (auto &nn:g[now]) {
+        for (auto &nn : g[now]) {
             int nxt = nn.first;
-            int nd = nn.second + dis;
+            ll nd = nn.second + dis;
             if (nd < rdist1[nxt]) {
                 rdist1[nxt] = nd;
-                q.push({nxt,nd}); 
+                q.push({nxt, nd});
             }
         }
     }
-    
-    priority_queue<pair<int,int>, vector<pair<int,int>>, cmp> qq;
 
-    qq.push({r2,0});
-    rdist2[r2] = 0; 
+    priority_queue<pair<int,ll>, vector<pair<int,ll>>, cmp> qq;
+    qq.push({r2, 0});
+    rdist2[r2] = 0;
 
-
-    while(!qq.empty()) {
+    while (!qq.empty()) {
         int now = qq.top().first;
-        int dis = qq.top().second;
+        ll dis = qq.top().second;
         qq.pop();
 
         if (rdist2[now] != dis) continue;
-        for (auto &nn:g[now]) {
+        for (auto &nn : g[now]) {
             int nxt = nn.first;
-            int nd = nn.second + dis;
+            ll nd = nn.second + dis;
             if (nd < rdist2[nxt]) {
                 rdist2[nxt] = nd;
-                qq.push({nxt,nd}); 
+                qq.push({nxt, nd});
             }
         }
     }
 
-    int mn = INT_MAX;
-    int rtor = rdist1[r2];
+    ll mn = LLONG_MAX;
+    ll rtor = rdist1[r2];
 
-    if (rtor == INT_MAX) {
+    if (rtor == LLONG_MAX) {
         cout << -1;
         return 0;
     }
 
-    for (int i = 1; i<=n; i++) {
-        int r1toi = rdist1[i];
-        int r2toi = rdist2[i];
+    for (int i = 1; i <= n; i++) {
+        ll r1toi = rdist1[i];
+        ll r2toi = rdist2[i];
 
-        if (r1toi == INT_MAX || r2toi == INT_MAX) continue;
+        if (r1toi == LLONG_MAX || r2toi == LLONG_MAX) continue;
 
-        mn = min(mn, r1toi+r2toi+rtor);
+        mn = min(mn, r1toi + r2toi + rtor);
     }
 
-    if (mn == INT_MAX) cout << -1;
+    if (mn == LLONG_MAX) cout << -1;
     else cout << mn;
-    // Please write your code here.
+
     return 0;
 }
